b0cf8ccf545a8baf16ee35e8a0b7b98bdcb9a8a3
saicharan14334 <vadanapusaicharan@gmail.com>
2025-01-09 11:52:48 +0530

    moving files to git

---
+++ Functions.js
@@ -0,0 +1,45 @@
+// function addToCart(){
+//     alert("Item added to cart");
+// }
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+// add(10,20);
+// add(100,31);
+// add(50,120);
+
+// function userDetails(name,age,city){
+//     console.log("Name: "+name);
+//     console.log("Age: "+age);
+//     console.log("City: "+city);
+// }
+
+// userDetails("Mahesh",23,"Pune");
+// userDetails("Ramesh",25,"Mumbai");
+// userDetails("Rajesh",29,"Delhi");
+
+// //! anonymous function
+// var add = function(a,b){
+//     console.log(a+b);
+// }
+// add(10,20);
+// add(100,31);
+// add(50,12);
+
+// //! IIFE -
+// (function(){
+//     console.log("IIFE");
+// })();
+
+// //arrow function
+// var demo=()=>console.log("Arrow function");
+
+// demo();
+
+var x=_=>{console.log("Arrow function1");
+    console.log("Hi");
+    console.log("Hello");
+    console.log("Bye");}
+    x();
---
+++ assignment2.js
@@ -0,0 +1,13 @@
+for(var i=0;i<5;i++){
+    for(var j=0;j<5;j++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+for(var i=0;i<6;i++){
+    for(var j=0;j<i;j++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
