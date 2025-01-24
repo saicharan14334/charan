ddb1ddcf0b2cc51a7a2f20701b3d440df1139775
saicharan14334 <vadanapusaicharan@gmail.com>
2025-01-24 12:56:49 +0530

    moving files to git

---
+++ dom.js
@@ -0,0 +1,37 @@
+// console.log(window);
+// console.log(document);
+
+// let element = document.getElementById("demo");
+// element.innerText = "G+"
+// console.log(element);
+
+// let ele = document.getElementById("test");
+// // ele.innerText= "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+  //!
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// // ele[0].style.backgroundColor="tamato"
+// //!spread operator:- it will takeout each and every value from the original array
+// //! and store it inside one more new array(pure array).
+// //? syntax :- []
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// x.map(element=>{
+//     //console.log(element);
+//     element.style.backgroundColor="orange"
+// })
+// let ele =document.querySelector('#mru');
+// console.log(ele);
+// let ele =document.querySelector('#mru');
+// // console.log(ele);
+// let ele =document.querySelectorAll('.test');
+// console.log(ele);
+let btn = document.querySelector('#btn');
+btn.addEventListener("click",()=>{
+    console.log("buttonclicked")
+
+})
