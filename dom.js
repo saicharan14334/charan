53f72249ec8f5e8a6362e2d482c4a1dd30261520
saicharan14334 <vadanapusaicharan@gmail.com>
2025-01-28 13:02:05 +0530

    moving files to git

--- dom.js
+++ dom.js
@@ -103,8 +103,55 @@
 // mainEle.appendChild(bottomEle);
 // document.body.appendChild(mainEle);
 
-let form = document.querySelector("form")
+let form = document.querySelector("form");
 
-let username = document.queryElementById("uName")
-let password = document.queryElementById("upass")
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
+
+
+let gender = document.getElementsByName("gender");
+// console.log(gender);
+check.addEventListener("click", event=>{
+  //  console.log(event.target.checked);
+  if(event.target.checked == true){
+    password.setAttribute("type", "text");
+    show.innerText= "hidepassword";
+
+
+  }else{
+     password.setAttribute("type", "password");
+    show.innerText= "showpassword";
+
+  }
+}
+ 
+)
+
+form.addEventListener("submit", event=>{
+  event.preventDefault();
+  let un = username.value;
+  let up = password.value;
+  let gen = "";
+
+  for(let i=0;i<=gender.length-1;i++){
+    // console.log(gender[i].value);
+    // console.log(gender[i].checked);
+    if(gender[i].checked==true){
+      gen=gender[i].value;
+    }
+  
+  }
+  let userDetails= {
+    username : un,
+    password : up,
+    gender : gen
+
+
+  }
+  console.log(userDetails);
+  sessionStorage.setItem("userData",JSON.stringify(userDetails))
+
+})
 
